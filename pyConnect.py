9cdc4261ae3219414cf6dc5c5456d49518ab6143
doct0rr <cleverkriss@gmail.com>
2017-11-08 23:43:47 +0530

    nearly done with getting pandefault and shit

--- pyConnect.py
+++ pyConnect.py
@@ -7,12 +7,13 @@ from threading import Event
 
 dataPoints = Queue()
 event = Event()
-
+defaultPanFlag = False
 #ser = serial.Serial('dev/ttyUSB0',115200,timeout=5)
 sleep(2)
-defaultSample = 400
+defaultSample = 100
 
 def dataFromSocket(inQ):
+    global defaultPanFlag
     soc = socket.socket()
     host = "192.168.1.169"
     port = 1990
@@ -20,6 +21,7 @@ def dataFromSocket(inQ):
     soc.listen(1)
     while(True):
         print("Online")
+        defaultPanFlag = True
         conn,addr = soc.accept()
         print("Connected")
         while(True):
@@ -27,10 +29,11 @@ def dataFromSocket(inQ):
             if not data:
                 conn.close()
                 print("Disconnected")
+                defaultPanFlag = True
                 break
             data = data.rstrip()
             data = data.decode("utf-8")
-            print(data)
+            #print(data)
             inQ.put(data)
             event.set()
 
@@ -38,12 +41,12 @@ def dataFromSocket(inQ):
 
 
 def mapper(x, inMin, inMax, outMin, outMax):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
 
 
 def panValues(defaultPan, x):
     left  = defaultPan - 156
-    right = defaultPna + 156
+    right = defaultPan + 156
     newX  = x
     if right > 313 and x < 0:
         newX = 626 + x
@@ -57,31 +60,56 @@ def tiltValue():
 
 def getDataFromQueue(outQ):
     event.wait()
-    if container.acquire(True) and not outQ.empty() :
+    if not outQ.empty() :
         data = outQ.get()
         datalist = data.split(",")
         x = datalist[0]
-        y = datalist[1]
-        event.clear()
-        return x, y
+        y = datalist[1].split("\n")
+        y = y[0]
+        if x!= "null" and y !="null":
+            try:
+                x = float(x)
+                y = float(y)
+                event.clear()
+                return (x, y)
+            except:
+                return (90, 60)
+        else:
+            return(90, 60)
     else:
         event.clear()
         return (90, 60)
 
-
+def getDefaultPan():
+        for i in range(100):
+            x,y = (getDataFromQueue(dataPoints))
+            defaultPan  = x
+        return defaultPan
 def loop():
+    global defaultPanFlag
+    defaultPan = getDefaultPan()
+    print(defaultPan)
     while True:
-        print(getDataFromQueue(dataPoints))
+        if defaultPanFlag:
+            defaultPan = getDefaultPan()
+            print(defaultPan)
+        x,y = (getDataFromQueue(dataPoints))
+        mappedX = panValues(defaultPan,x)
+        print("mappedX: ",mappedX,"X: ",x,"Y: ",y)
+        defaultPanFlag = False
 
 
 def worker():
+    print("ok ?")
     for i in range(defaultSample):
         x,y    = getDataFromQueue(dataPoints)
         defaultPan  = x
         defaultTilt = y
     while True:
+        print("HERE")
         x,y = getDataFromQueue(dataPoints)
-
+        mappedX = panValues(defaultPan,x)
+        print(mappedX,y)
 
 
 
@@ -96,6 +124,5 @@ if __name__ == '__main__':
     t1 = threading.Thread(target = dataFromSocket, args = (dataPoints, ))
     t2 = threading.Thread(target = loop)
 
-    t1.start()
-    sleep(5)
     t2.start()
+    t1.start()
